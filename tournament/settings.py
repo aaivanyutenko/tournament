5f94553013fcc3ba0d6f57099c344f68917f29bd
Anton Ivanyutenko <anton.ivanyutenko@gmail.com>
2014-11-21 17:09:47 +0300

    Setting remote MySQL database

--- tournament/settings.py
+++ tournament/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'tournament.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tour_galaxy',
+        'USER': 'skywalker',
+        'PASSWORD': '34rf451dWE23r4',
+        'HOST': '5.231.39.118',
+        'PORT': '3306',
     }
 }
 
